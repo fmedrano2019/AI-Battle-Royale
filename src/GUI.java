7902823b779e0b765d5179559d6234c3c0d3c9ea
fmedrano2019 <37252092+fmedrano2019@users.noreply.github.com>
2019-05-22 18:13:35 -0400

    Add files via upload

--- src/GUI.java
+++ src/GUI.java
@@ -4,16 +4,26 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GUI extends JPanel {
+	
 	//area
 	private JButton[][] map;
 	private int[][] cells;
+	/**
+	 * 0 = grass
+	 * 1 = water
+	 * 2 = tree
+	 * 3 = food
+	 * 4 = player alive
+	 * 5 = player dead
+	 */
+	
 	
 	//leaderboard
-	private JLabel numPlayers;
-	private JList<String> playerNames;
+	private JLabel numPlayers; //shows number of players alive
+	private JList<String> playerNames; //lists player names
 	private JButton reset;
 	
-	private ArrayList<Player> players;
+	private ArrayList<Player> players; //stores the players
 	private ArrayList<String> nameGen = new ArrayList<String>(); //name generator
 	private ArrayList<String> typeGen = new ArrayList<String>(); //type generator
 	
@@ -24,20 +34,24 @@ public class GUI extends JPanel {
 		area.setLayout(new GridLayout(100, 100));
 		add(area, BorderLayout.CENTER);
 		map = new JButton[100][100];
-		
+		cells = new int[100][100];
 		
 		for(int r = 0; r < map.length; r++) {
 			for(int c = 0; c < map[0].length; c++) {
 				map[r][c] = new JButton();
-				map[r][c].setBackground(Color.green);
+				map[r][c].setBackground(new Color(47, 230, 36));
 				area.add(map[r][c]);
+				cells[r][c] = 0;
 			}
 		}
+		waterPlacement(); //generates bodies of water
 		
 		nameGen(); //adds names to nameGen
 		typeGen(); //adds types to typeGen
 		players = new ArrayList<Player>();
-		playerCreation();
+		playerCreation(); //adds players
+		
+		playerPlacement(); //generates players
 		
 		JPanel leaderboard = new JPanel(); //leaderboard
 		leaderboard.setLayout(new BorderLayout());
@@ -46,7 +60,7 @@ public class GUI extends JPanel {
 		numPlayers = new JLabel("Number of players left: " + countAlive());
 		leaderboard.add(numPlayers, BorderLayout.NORTH);
 		
-		playerNames = playerNameList();
+		playerNames = new JList<String>(playerNameList());
 		leaderboard.add(playerNames, BorderLayout.CENTER);
 		
 		reset = new JButton("Reset");
@@ -54,14 +68,72 @@ public class GUI extends JPanel {
 		leaderboard.add(reset, BorderLayout.SOUTH);
 	}
 	
-	//list of players
-	public JList<String> playerNameList() {
+	//post: generates 20 bodies of water
+	public void waterPlacement() {
+		int num = (int)(Math.random() * 6 + 10); //number of bodies of water 10-15
+		for(int c = 0; c < num; c++) {
+			waterBodyGen();
+		}
+	}
+	
+	//method of waterGen()
+	//post: generates a 4x4 body of water
+	private void waterBodyGen() {
+		int x = (int)(Math.random() * map.length);
+		int y = (int)(Math.random() * map[0].length);
+		while(x < 0 || x >= map.length || y < 0 || y >= map[0].length || cells[y][x] == 1) { //checks if coordinates are within bounds and aren't on top of water
+			x = (int)(Math.random() * map.length);
+			y = (int)(Math.random() * map[0].length);
+		} //generates new coordinates
+		
+		for(int r = y; r < y + 5; r++) {
+			for(int c = x ; c < x + 5; c++) {
+				if(r < map.length && c < map[0].length) { //checks if r & c are within bounds
+					map[r][c].setBackground(new Color(26, 23, 224));
+					cells[r][c] = 1;
+				}
+			}
+		}
+	}
+	
+	//post: places players on the map
+	public void playerPlacement() {
+		for(int c = 0; c < players.size(); c++) {
+			int x = (int)(Math.random() * map.length);
+			int y = (int)(Math.random() * map[0].length);
+			while(x < 0 || x >= map.length || y < 0 || y >= map[0].length || cells[y][x] == 1 || detectPlayers(x, y)) { //checks if coordinates are within bounds and aren't on top of water
+				x = (int)(Math.random() * map.length);
+				y = (int)(Math.random() * map[0].length);
+			} //generates new coordinates
+		
+			cells[y][x] = 4;
+			map[y][x].setBackground(Color.red);
+		}
+	}
+	
+	//method of playerPlacement()
+	//post: returns if there is a player within a 5x5 square of the given coordinates
+	private boolean detectPlayers(int x, int y) {
+		for(int r = y - 5; r <= y + 5; r++) {
+			for(int c = x - 5; c <= x + 5; c++) {
+				if(r >= 0 && r < map.length && c >= 0 && c < map[0].length) { //checks if r & c are within bounds
+					if(cells[r][c] == 4) //if there is a player
+						return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	//post: returns an array of the player names
+	public String[] playerNameList() {
 		String[] n = new String[players.size()];
 		for(int c = 0; c < players.size(); c++) {
 			n[c] = players.get(c).toString();
 		}
 		
-		return new JList(n);
+		return n;
 	}
 	
 	//name generator
@@ -159,6 +231,7 @@ public class GUI extends JPanel {
 			else {
 				p = new SwordAndShield(name);
 			}
+			
 			players.add(p);
 		}
 	}
@@ -166,8 +239,8 @@ public class GUI extends JPanel {
 	//post: returns how many players are alive
 	public int countAlive() {
 		int count = 0;
-		for(int c = 0; c < players.size(); c++) {
-			if(players.get(c).isAlive)
+		for(int c = 0; c < players.size(); c++) { //iterates thru players
+			if(players.get(c).isAlive) //if they are alive
 				count++;
 		}
 		
@@ -180,6 +253,16 @@ public class GUI extends JPanel {
 			players.clear(); //clears all players
 			playerCreation(); //adds 20 players
 			numPlayers.setText("Number of players left: " + countAlive());
+			playerNames.setListData(playerNameList());
+			
+			for(int r = 0; r < map.length; r++) {
+				for(int c = 0; c < map[0].length; c++) {
+					map[r][c].setBackground(new Color(47, 230, 36));
+					cells[r][c] = 0;
+				}
+			} //clears map
+			waterPlacement(); //generates bodies of water
+			playerPlacement();
 		}
 	}
 }
