a6291725534587625d816d152879a0c0ffc0ffc9
fmedrano2019 <37252092+fmedrano2019@users.noreply.github.com>
2019-05-27 16:38:03 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 
-public abstract class Player
-{
+public abstract class Player {
 	
 	public String name;
 	public String type;
@@ -11,15 +10,15 @@ public abstract class Player
 	public int food;
 	public int water;
 	public int energy;
-   public int x;
-   public int y;
+	public int x;
+	public int y;
 	public boolean isAlive;
-   public boolean active;
+	public boolean active;
 	
 	/** CONSTRUCTORS **/
 	
-   //no coordinate constructor
-   public Player(String n, String t) {
+	//no coordinate constructor
+	public Player(String n, String t) {
 		name = n;
 		type = t;
 		kills = 0;
@@ -28,10 +27,10 @@ public abstract class Player
 		water = 60;
 		energy = food + water;
 		isAlive = true;
-      active = true;
+		active = true;
 	}
-   
-   //constructor with coordinate
+
+	//constructor with coordinate
 	public Player(String n, String t, int x, int y) {
 		name = n;
 		type = t;
@@ -40,48 +39,48 @@ public abstract class Player
 		food = 40;
 		water = 60;
 		energy = food + water;
-      this.x=x;
-      this.y=y;
+		this.x = x;
+		this.y = y;
 		isAlive = true;
-      active = true; 
+		active = true;
 	}
-	
+
 	/** ACCESSORS **/
 	
 	//post: returns name
 	public String getName() {
 		return name;
 	}
-   
-   //post: returns only the first name
-   public String getFirstName()
-   {
-      return name.substring(0, name.indexOf(" "));
-   }
-   
-   //post: sets first name
-   public void setFirstName(String n)
-   {
-      name = n + name.substring(name.indexOf(" "));
-   }
-   
-   //post: returns only the last name
-   public String getLastName()
-   {
-      return name.substring(name.indexOf(" ")+1);
-   }
-   
-   //post: sets only last name
-   public void setLastName(String n)
-   {
-      name=name.substring(0, name.indexOf(" ")+1) + n;
-   }
-   
+	
 	//post: sets name
 	public void setName(String n) {
 		name = n;
 	}
 
+	//post: returns only the first name
+   	public String getFirstName()
+	{
+		return name.substring(0, name.indexOf(" "));
+	}
+
+	//post: sets first name
+	public void setFirstName(String n)
+	{
+   		name = n + name.substring(name.indexOf(" "));
+	}
+
+	//post: returns only the last name
+	public String getLastName()
+	{
+		return name.substring(name.indexOf(" ")+1);
+	}
+
+	//post: sets only last name
+	public void setLastName(String n)
+	{
+		name=name.substring(0, name.indexOf(" ")+1) + n;
+	}
+	
 	//post: returns type
 	public String getType() {
 		return type;
@@ -145,31 +144,26 @@ public abstract class Player
 	public void setEnergy() {
 		energy = water + food;
 	}
-   
-   //post: returns the x coordinate
-   public int getX()
-   {
-      return x;
-   }
-   
-   //post: sets the x coordinate
-   public void setX(int v)
-   {
-      x=v;
-   }
-   
-   //post: returns the y coordinate
-   public int getY()
-   {
-      return y;
-   }
-   
-   
-   //post: sets the y coordinate
-   public void setY(int v)
-   {
-      y=v;
-   }
+	
+	//post: returns the x coordinate
+	public int getX() {
+		return x;
+	}
+
+	//post: sets the x coordinate
+	public void setX(int v) {
+		x = v;
+	}
+
+	//post: returns the y coordinate
+	public int getY() {
+		return y;
+	}
+
+	//post: sets the y coordinate
+	public void setY(int v) {
+		y = v;
+	}
 	
 	//post: returns isAlive
 	public boolean isAlive() {
@@ -178,146 +172,145 @@ public abstract class Player
 	
 	//post: sets isAlive
 	public void setAlive(boolean a) {
+		int [][] cells=GUI.getCells();
+		JButton [][] map=GUI.getMap();
 		isAlive = a;
 		if(!a) {
 			active = false;
-			GUI.cells[y][x] = 7;
-			GUI.map[y][x].setBackground(Color.black);
+			cells[y][x] = 7;
+			map[y][x].setBackground(Color.black);
 		}
 	}
-   
-   //post: returns true if the player has not done something that turn yet, false if they have
-   public boolean isActive()
-   {
-      return active;
-   }
-   
-   //post: sets active to boolean a
-   public void setActive(boolean a)
-   {
-      active=a;
-   }
-   
-   public int superiorType(Player other)
-   {
-      if(type.equals(other.getType()))
-         return 0;
-      else if((type.equals("S&S")&&other.getType().equals("2H"))||(type.equals("2H")&&other.getType().equals("Dagger"))||(type.equals("Dagger")&&other.getType().equals("S&S")))
-         return 50;
-      else
-         return -50;
-   }
-   
-   public boolean isWinner(Player other)
-   {
-      double score=0;
-      double otherScore=0;
-      
-      int typeWinner=superiorType(other);
-      
-      score+=typeWinner;
-      otherScore+=typeWinner;
-      
-      double energyDifference=0;
-      if(energy>other.getEnergy())
-         energyDifference=energy-other.getEnergy();
-      else
-         energyDifference=other.getEnergy()-energy;
-        
-      energyDifference=Math.exp(energyDifference);
-      energyDifference=Math.pow(energyDifference, 1.0/45.0);
-        
-      score+=energyDifference;
-      otherScore-=energyDifference;
-      
-      double xpDifference=0;
-      if(xp>other.getXP())
-         xpDifference=xp-other.getXP();
-      else
-         xpDifference=other.getXP()-xp;
-        
-      xpDifference=Math.exp(xpDifference);
-      xpDifference=Math.pow(xpDifference, 1.0/60.0);
-        
-      score+=xpDifference;
-      otherScore-=xpDifference;
-         
-      return score>=otherScore;
-   }
-  
-   public void moveUp()
-   {  
-      int [][] cells=GUI.getCells();
-      JButton [][] map=GUI.getMap();
-      if(y > 0 && GUI.getSpace(x, y-1)==0)
-      {
-         cells[y-1][x] = GUI.getSpace(x, y);
-         cells[y][x] = 0;
-         map[y][x].setBackground(new Color(47, 230, 36));
-         map[y-1][x].setBackground(this.getColor());
-         y--;
-      }
-   }
-   
-   public void moveDown()
-   {
-      int [][] cells=GUI.getCells();
-      JButton [][] map=GUI.getMap();
-      if(y < map.length-1 && GUI.getSpace(x, y+1)==0)
-      {
-         cells[y+1][x] = GUI.getSpace(x, y);
-         cells[y][x] = 0;
-         map[y][x].setBackground(new Color(47, 230, 36));
-         map[y+1][x].setBackground(this.getColor());
-         y++;
-      }
-   }
-   
-   public void moveLeft()
-   {  
-      int [][] cells=GUI.getCells();
-      JButton [][] map=GUI.getMap();
-      if(x > 0 && GUI.getSpace(x-1, y)==0)
-      {
-         cells[y][x-1] = GUI.getSpace(x, y);
-         cells[y][x] = 0;
-         map[y][x].setBackground(new Color(47, 230, 36));
-         map[y][x-1].setBackground(this.getColor());
-         x--;
-      }
-   }
-   
-   public void moveRight()
-   {
-      int [][] cells=GUI.getCells();
-      JButton [][] map=GUI.getMap();
-      if(x < map[0].length-1 && GUI.getSpace(x+1, y)==0)
-      {
-         cells[y][x+1] = GUI.getSpace(x, y);
-         cells[y][x] = 0;
-         map[y][x].setBackground(new Color(47, 230, 36));
-         map[y][x+1].setBackground(this.getColor());
-         x++;
-      }
-   }
-   
-   
-   public void eat()
-   {
-      food+=15;
-      energy+=15;
-      xp += 5;
-   }
-   
-   public void drink()
-   {
-      water+=20;
-      energy+=20;
-      xp += 10;
-   }   
 	
-   public abstract Color getColor();
-   
+	//post: returns true if the player has not done something that turn yet, false if they have
+	public boolean isActive() {
+		return active;
+	}
+
+	//post: sets active to boolean a
+	public void setActive(boolean a) {
+		active = a;
+	}
+	
+	public int superiorType(Player other)
+	{
+		if(type.equals(other.getType()))
+			return 0;
+		else if((type.equals("S&S")&&other.getType().equals("2H"))||(type.equals("2H")&&other.getType().equals("Dagger"))||(type.equals("Dagger")&&other.getType().equals("S&S")))
+			return 50;
+		else
+			return -50;
+	}
+
+	public boolean isWinner(Player other)
+	{
+		double score=0;
+		double otherScore=0;
+
+		int typeWinner=superiorType(other);
+
+		score+=typeWinner;
+		otherScore+=typeWinner;
+
+		double energyDifference=0;
+		if(energy>other.getEnergy())
+			energyDifference=energy-other.getEnergy();
+		else
+			energyDifference=other.getEnergy()-energy;
+
+		energyDifference=Math.exp(energyDifference);
+		energyDifference=Math.pow(energyDifference, 1.0/45.0);
+
+		score+=energyDifference;
+		otherScore-=energyDifference;
+
+		double xpDifference=0;
+		if(xp>other.getXP())
+			xpDifference=xp-other.getXP();
+		else
+			xpDifference=other.getXP()-xp;
+
+		xpDifference=Math.exp(xpDifference);
+		xpDifference=Math.pow(xpDifference, 1.0/60.0);
+
+		score+=xpDifference;
+		otherScore-=xpDifference;
+
+		return score>=otherScore;
+	}
+
+	public void moveUp()
+	{  
+		int [][] cells=GUI.getCells();
+		JButton [][] map=GUI.getMap();
+		if(y > 0 && GUI.getSpace(x, y-1)==0)
+		{
+			cells[y-1][x] = GUI.getSpace(x, y);
+			cells[y][x] = 0;
+			map[y][x].setBackground(new Color(47, 230, 36));
+			map[y-1][x].setBackground(this.getColor());
+			y--;
+		}
+	}
+
+	public void moveDown()
+	{
+		int [][] cells=GUI.getCells();
+		JButton [][] map=GUI.getMap();
+		if(y < map.length-1 && GUI.getSpace(x, y+1)==0)
+		{
+			cells[y+1][x] = GUI.getSpace(x, y);
+			cells[y][x] = 0;
+			map[y][x].setBackground(new Color(47, 230, 36));
+			map[y+1][x].setBackground(this.getColor());
+			y++;
+		}
+	}
+
+	public void moveLeft()
+	{  
+		int [][] cells=GUI.getCells();
+		JButton [][] map=GUI.getMap();
+		if(x > 0 && GUI.getSpace(x-1, y)==0)
+		{
+			cells[y][x-1] = GUI.getSpace(x, y);
+			cells[y][x] = 0;
+			map[y][x].setBackground(new Color(47, 230, 36));
+			map[y][x-1].setBackground(this.getColor());
+			x--;
+		}
+	}
+
+	public void moveRight()
+	{
+		int [][] cells=GUI.getCells();
+		JButton [][] map=GUI.getMap();
+		if(x < map[0].length-1 && GUI.getSpace(x+1, y)==0)
+		{
+			cells[y][x+1] = GUI.getSpace(x, y);
+			cells[y][x] = 0;
+			map[y][x].setBackground(new Color(47, 230, 36));
+			map[y][x+1].setBackground(this.getColor());
+			x++;
+		}
+	}
+
+	public void eat()
+	{
+		food+=15;
+		energy+=15;
+		xp += 5;
+	}
+
+	public void drink()
+	{
+		water+=20;
+		energy+=20;
+		xp += 10;
+	}   
+
+	public abstract Color getColor();
+	
 	public String toString() {
 		return name + " (" + type + ") " + ": " + kills + " - Food: " + food + " - Water: " + water + " - XP: " + xp;
 	}
